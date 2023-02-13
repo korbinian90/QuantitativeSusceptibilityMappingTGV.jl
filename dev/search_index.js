da7c9254f39cf46ca5d86b0b504534eabd911654
Documenter.jl <documenter@juliadocs.github.io>
2023-02-13 14:42:45 +0000

    build based on 1dbafa7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TGV_QSM","category":"page"},{"location":"#TGV_QSM","page":"Home","title":"TGV_QSM","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TGV_QSM.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TGV_QSM]","category":"page"}]
+}

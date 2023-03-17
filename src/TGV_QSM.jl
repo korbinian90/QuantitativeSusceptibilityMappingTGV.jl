6db230bfedc0cf5e32b1a71933759685f7a7caf3
korbinian90 <korbinian90@gmail.com>
2023-03-17 15:52:01 +0100

    add 3D resampling functions; (not yet using nifti information)

--- src/TGV_QSM.jl
+++ src/TGV_QSM.jl
@@ -1,10 +1,11 @@
 module TGV_QSM
 
-using CUDA, CUDAKernels, KernelAbstractions, PaddedViews, ImageMorphology
+using CUDA, CUDAKernels, KernelAbstractions, PaddedViews, ImageMorphology, Interpolations, Rotations, OffsetArrays, StaticArrays
 
 include("tgv.jl")
 include("tgv_helper.jl")
+include("resample.jl")
 
-export qsm_tgv, get_laplace_phase3, laplacian
+export qsm_tgv, get_laplace_phase3, laplacian, rotate3D
 
 end

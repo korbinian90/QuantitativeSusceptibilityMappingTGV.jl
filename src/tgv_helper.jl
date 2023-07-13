1fd76c0977e1f27a4462573daa680f40e373bb15
korbinian90 <korbinian90@gmail.com>
2023-07-13 13:56:27 +1000

    refactor indices; works only on gpu

--- src/tgv_helper.jl
+++ src/tgv_helper.jl
@@ -1,4 +1,5 @@
-@inline function stencil(((i, j, k), (x, y, z)), A, mask)
+@inline function stencil((I, (x, y, z)), A, mask)
+    i, j, k = Tuple(I)
     @inbounds begin
         A0 = A[i, j, k]
         if isnothing(mask)
@@ -8,7 +9,7 @@
             A2p = (j < y) ? A[i, j+1, k] : A0
             A3m = (k > 1) ? A[i, j, k-1] : A0
             A3p = (k < z) ? A[i, j, k+1] : A0
-        else # TODO test to switch mask to &&
+        else # TODO test to switch mask to && (also in div_local)
             A1m = (i > 1) ? mask[i-1, j, k] * A[i-1, j, k] : A0
             A1p = (i < x) ? mask[i+1, j, k] * A[i+1, j, k] : A0
             A2m = (j > 1) ? mask[i, j-1, k] * A[i, j-1, k] : A0
@@ -20,7 +21,6 @@
     return A0, A1m, A1p, A2m, A2p, A3m, A3p
 end
 
-# TODO try to rotate all stencils with omega
 @inline function filter_local(I, A, w, mask=nothing)
     A0, A1m, A1p, A2m, A2p, A3m, A3p = stencil(I, A, mask)
 
@@ -29,7 +29,8 @@ end
     (-2A0 + A3m + A3p) * w[3]
 end
 
-@inline function div_local(((i, j, k), (x, y, z)), A::AbstractArray{T}, (r1, r2, r3), mask, (i1, i2, i3)=(1, 2, 3)) where {T}
+@inline function div_local((I, (x, y, z)), A::AbstractArray{T}, (r1, r2, r3), mask, (i1, i2, i3)=(1, 2, 3)) where {T}
+    i, j, k = Tuple(I)
     @inbounds begin
         div = (i < x) ? mask[i, j, k] * A[i1, i, j, k] * r1 : zero(T)
         div -= (i > 1) ? mask[i-1, j, k] * A[i1, i-1, j, k] * r1 : zero(T)
@@ -43,7 +44,8 @@ end
     return div
 end
 
-function symgrad_local(((i, j, k), (x, y, z)), A::AbstractArray{T}, resinv) where {T}
+function symgrad_local((I, (x, y, z)), A::AbstractArray{T}, resinv) where {T}
+    i, j, k = Tuple(I)
     @inbounds begin
         if (i < x)
             wxx = resinv[1] * (A[1, i+1, j, k] - A[1, i, j, k])
@@ -77,27 +79,30 @@ end
 
 # Update eta <- eta + sigma*mask*(-laplace(phi) + wave(chi) - laplace_phi0). 
 @kernel function update_eta_kernel!(eta, phi, chi, laplace_phi0, mask, sigma, laplace_kernel, dipole_kernel)
-    I = @index(Global, Cartesian)
-    R = @ndrange
-    laplace = filter_local((Tuple(I), R), phi, laplace_kernel)
-    wave = filter_local((Tuple(I), R), chi, dipole_kernel)
+    I, R = @index(Global, Cartesian), @ndrange
+
+    laplace = filter_local((I, R), phi, laplace_kernel)
+    wave = filter_local((I, R), chi, dipole_kernel)
 
     @inbounds eta[I] += sigma * mask[I] * (-laplace + wave - laplace_phi0[I])
 end
 
+@inline function grad_local((I, (x, y, z)), A::AbstractArray{T}, resinv) where {T}
+    A0 = A[i, j, k]
+    dx = (i < x) ? (A[i+1, j, k] - A0) * resinv[1] : zero(T)
+    dy = (j < y) ? (A[i, j+1, k] - A0) * resinv[2] : zero(T)
+    dz = (k < z) ? (A[i, j, k+1] - A0) * resinv[3] : zero(T)
+    return dx, dy, dz
+end
+
 # Update p <- P_{||.||_\infty <= alpha}(p + sigma*(mask0*grad(phi_f) - mask*w). 
 @inline norm((x, y, z)) = sqrt(x * x + y * y + z * z)
-@kernel function update_p_kernel!(p::AbstractArray{T}, chi, w, mask, mask0, sigma, alphainv, resinv) where {T}
-    I = @index(Global, Cartesian)
-    i,j,k = Tuple(I)
-    x, y, z = @ndrange
+@kernel function update_p_kernel!(p, chi, w, mask, mask0, sigma, alphainv, resinv)
+    I, R = @index(Global, Cartesian), @ndrange
+
+    dxp, dyp, dzp = grad_local((I, R), chi, resinv)
+
     @inbounds begin
-        chi0 = chi[I]
-
-        dxp = (i < x) ? (chi[i+1, j, k] - chi0) * resinv[1] : zero(T)
-        dyp = (j < y) ? (chi[i, j+1, k] - chi0) * resinv[2] : zero(T)
-        dzp = (k < z) ? (chi[i, j, k+1] - chi0) * resinv[3] : zero(T)
-
         sigmaw0 = sigma * mask0[I]
         sigmaw = sigma * mask[I]
 
@@ -117,12 +122,12 @@ end
 # Update q <- P_{||.||_\infty <= alpha}(q + sigma*weight*symgrad(u)).
 @inline qnorm((xx, xy, xz, yy, yz, zz)) = sqrt(xx * xx + yy * yy + zz * zz + 2 * (xy * xy + xz * xz + yz * yz))
 @kernel function update_q_kernel!(q, u, weight, sigma, alphainv, resinv)
-    I = @index(Global, Cartesian)
-    R = @ndrange
+    I, R = @index(Global, Cartesian), @ndrange
+
+    wxx, wxy, wxz, wyy, wyz, wzz = symgrad_local((I, R), u, resinv)
+
     @inbounds begin
         sigmaw = sigma * weight[I]
-
-        wxx, wxy, wxz, wyy, wyz, wzz = symgrad_local((Tuple(I), R), u, resinv)
 
         q[1, I] += sigmaw * wxx
         q[2, I] += sigmaw * wxy
@@ -145,39 +150,39 @@ end
 
 # Update phi_dest <- (phi + tau*laplace(mask0*eta))/(1+mask*tau). 
 @kernel function update_phi_kernel!(phi_dest::AbstractArray{T}, phi, eta, mask, mask0, tau, laplace_kernel) where {T}
-    I = @index(Global, Cartesian)
-    R = @ndrange
-    laplace = filter_local((Tuple(I), R), eta, laplace_kernel, mask0)
-    phi_dest[I] = (phi[I] + tau * laplace) / (one(T) + mask[I] * tau)
+    I, R = @index(Global, Cartesian), @ndrange
+
+    laplace = filter_local((I, R), eta, laplace_kernel, mask0)
+    @inbounds phi_dest[I] = (phi[I] + tau * laplace) / (one(T) + mask[I] * tau)
 end
 
 # Update chi_dest <- chi + tau*(div(p) - wave(mask*v)). 
 @kernel function update_chi_kernel!(chi_dest, chi, v, p, mask0, tau, resinv, dipole_kernel)
-    I = @index(Global, Cartesian)
-    R = @ndrange
-    div = div_local((Tuple(I), R), p, resinv, mask0)
-    wave = filter_local((Tuple(I), R), v, dipole_kernel, mask0)
+    I, R = @index(Global, Cartesian), @ndrange
+
+    div = div_local((I, R), p, resinv, mask0)
+    wave = filter_local((I, R), v, dipole_kernel, mask0)
 
     @inbounds chi_dest[I] = chi[I] + tau * (div - wave)
 end
 
 # Update w_dest <- w + tau*(mask*p + div(mask0*q)). 
-@kernel function update_w_kernel!(w_dest::AbstractArray{T}, w, p, q, mask, mask0, tau, (r1, r2, r3)) where {T}
-    I = @index(Global, Cartesian)
-    R = @ndrange
-    # @inbounds begin
+@kernel function update_w_kernel!(w_dest, w, p, q, mask, mask0, tau, (r1, r2, r3))
+    I, R = @index(Global, Cartesian), @ndrange
+
+    @inbounds begin
         w_dest[1, I] = w[1, I]
         w_dest[2, I] = w[2, I]
         w_dest[3, I] = w[3, I]
         if mask[I]
-            q123 = div_local((Tuple(I), R), q, (r1, r1, r1), mask0, (1, 2, 3))
-            q245 = div_local((Tuple(I), R), q, (r2, r2, r2), mask0, (2, 4, 5))
-            q356 = div_local((Tuple(I), R), q, (r3, r3, r3), mask0, (3, 5, 6))
+            q123 = div_local((I, R), q, (r1, r1, r1), mask0, (1, 2, 3))
+            q245 = div_local((I, R), q, (r2, r2, r2), mask0, (2, 4, 5))
+            q356 = div_local((I, R), q, (r3, r3, r3), mask0, (3, 5, 6))
             w_dest[1, I] += tau * (p[1, I] + q123)
             w_dest[2, I] += tau * (p[2, I] + q245)
             w_dest[3, I] += tau * (p[3, I] + q356)
         end
-    # end
+    end
 end
 
 function extragradient_update!(u_, u)

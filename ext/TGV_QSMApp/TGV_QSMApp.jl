d4136a8da390291a726455f81c6e76d850546e2b
korbinian90 <korbinian90@gmail.com>
2023-10-11 16:27:12 +1000

    add app basic structure

---
+++ ext/TGV_QSMApp/TGV_QSMApp.jl
@@ -0,0 +1,10 @@
+module RomeoApp
+
+using ArgParse
+using MriResearchTools
+using TGV_QSM
+
+include("argparse.jl")
+include("caller.jl")
+
+end # module
--- src/QuantitativeSusceptibilityMappingTGV.jl
+++ src/QuantitativeSusceptibilityMappingTGV.jl
@@ -6,6 +6,8 @@ include("tgv.jl")
 include("tgv_helper.jl")
 include("laplacian.jl")
 
-export qsm_tgv, get_laplace_phase3, get_laplace_phase_del, get_laplace_phase_romeo
+TGV_QSM_main(args...; kwargs...) = @warn("Type `using ArgParse` to use this function \n `?TGV_QSM_main` for argument help")
+
+export qsm_tgv, get_laplace_phase3, get_laplace_phase_del, get_laplace_phase_romeo, TGV_QSM_main
 
 end

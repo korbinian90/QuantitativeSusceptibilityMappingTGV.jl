cb3d86ec0a3c7e9346ed7e7e1890e48530c10588
Korbinian Eckstein <korbinian.eckstein@gmail.com>
2023-10-17 14:14:57 +1000

    add LinearAlgebra package

--- src/QuantitativeSusceptibilityMappingTGV.jl
+++ src/QuantitativeSusceptibilityMappingTGV.jl
@@ -1,6 +1,6 @@
 module QuantitativeSusceptibilityMappingTGV
 
-using CUDA, KernelAbstractions, PaddedViews, ImageMorphology, Interpolations, Rotations, OffsetArrays, StaticArrays, ProgressMeter, Statistics, ImageFiltering, ROMEO
+using CUDA, KernelAbstractions, PaddedViews, ImageMorphology, Interpolations, Rotations, OffsetArrays, StaticArrays, ProgressMeter, Statistics, ImageFiltering, ROMEO, LinearAlgebra
 
 include("tgv.jl")
 include("tgv_helper.jl")

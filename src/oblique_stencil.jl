ce3607f0de69b429865ed1d66b73c40a0ff7f878
Korbinian Eckstein <korbinian.eckstein@gmail.com>
2023-12-19 14:04:42 +1000

    new stencil for anisotropic; not working with rotation and anisotropy yet

--- src/oblique_stencil.jl
+++ src/oblique_stencil.jl
@@ -1,18 +1,21 @@
 using ImageFiltering, FFTW, LinearAlgebra
 
-function dipole(x, y, z, direction=(0, 0, 1))
+function dipole(x, y, z, r_treshold, direction=(0, 0, 1))
     r = sqrt(x * x + y * y + z * z)
+    if r < r_treshold
+        return NaN
+    end
     xz = (direction[1] * x + direction[2] * y + direction[3] * z) / r
     kappa = 1 / 4pi * (3 * xz * xz - 1) / (r * r * r)
     return kappa
 end
 
-function stencil(; st=27, res=(1.0, 1.0, 1.0), direction=(0, 0, 1), gridsize=(128, 128, 128))
+function stencil(; st=27, res=(1.0, 1.0, 1.0), res0=1.0, direction=(0, 0, 1), gridsize=(128, 128, 128))
     middle = floor.(Int, gridsize ./ 2) .+ 1
 
-    coord = [((1:gridsize[i]) .- middle[i]) * res[i] for i in 1:3]
+    coord = [((1:gridsize[i]) .- middle[i]) * res0 for i in 1:3]
 
-    d = [dipole(x, y, z, direction) for x in coord[1], y in coord[2], z in coord[3]]
+    d = [dipole(x, y, z, 4res0, direction) for x in coord[1], y in coord[2], z in coord[3]]
     d_mask = isfinite.(d)
 
     # stencil mask
@@ -25,6 +28,7 @@ function stencil(; st=27, res=(1.0, 1.0, 1.0), direction=(0, 0, 1), gridsize=(12
         mask = centered(falses((3, 3, 3)))
         mask[0, 0, :] .= mask[0, :, 0] .= mask[:, 0, 0] .= true
     end
+    mask[0,0,0] = false
 
     midInd = CartesianIndex(middle)
 
@@ -33,14 +37,16 @@ function stencil(; st=27, res=(1.0, 1.0, 1.0), direction=(0, 0, 1), gridsize=(12
     idst(x) = FFTW.r2r(x, FFTW.RODFT00) ./ prod(2 .* (size(x) .+ 1))
 
     coord2 = [2 * sinpi.(((0:N-1) .+ 1) / (2 * (N + 1))) for N in gridsize]
-    coord2_grid = ((coord2[1] .^ 2) ./ (res[1]^2) .+ reshape((coord2[2] .^ 2) ./ (res[2]^2), 1, :) .+ reshape((coord2[3] .^ 2) ./ (res[3]^2), 1, 1, :))
+    coord2_grid = (coord2[1] .^ 2) .+ reshape(coord2[2] .^ 2, 1, :) .+ reshape(coord2[3] .^ 2, 1, 1, :)
     vdeltas = []
     for I in CartesianIndices(mask)
         if !mask[I]
             continue
         end
         delta = zeros(gridsize)
-        delta[I+midInd] = 1
+        delta[midInd+I] = 1
+        delta[midInd-I] = 1
+        delta[midInd] = -2
         Fdelta = dst(delta)
         Fdelta ./= -coord2_grid
         vdelta = idst(Fdelta)
@@ -57,16 +63,22 @@ function stencil(; st=27, res=(1.0, 1.0, 1.0), direction=(0, 0, 1), gridsize=(12
 
     y = F.Vt * d[d_mask]
     x = F.U * (y .* s_inv)
-    x_corr = x * prod(res)
+    x_corr = x * res0^3
 
-    stencil = zeros(Float32, 3, 3, 3)
+    stencil = zeros(Float32, 3, 3, 3)    
+    
     ind = 1
     for i in eachindex(stencil)
         if mask[i]
-            stencil[i] = x_corr[ind]
+            stencil[i] = 2x_corr[ind]
             ind += 1
         end
     end
+    weights = [(i^2 / res[1]^2 + j^2 / res[2]^2 + k^2 / res[3]^2) / (i^2 + j^2 + k^2) for i in -1:1, j in -1:1, k in -1:1]
+    stencil .*= weights
+    
+    stencil[2,2,2] = 0
+    stencil[2,2,2] = -sum(stencil)
 
     return stencil
 end
